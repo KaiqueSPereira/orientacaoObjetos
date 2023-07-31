e0073c2a07d42f468224bb46ede5a0bab003a4fe
kaique_kdsp@live.com <kaique_kdsp@live.com>
2023-07-31 20:38:39 -0300

    Incio do controle de verção

---
+++ src/main/java/br/com/exemplo/orientecaoObjetos/OrientecaoObjetosApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.orientecaoObjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientecaoObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientecaoObjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exemplo/orientecaoObjetos/OrientecaoObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.orientecaoObjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientecaoObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
