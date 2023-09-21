db3ebed41842fdc7756630bbfcf12f7394407c63
kaique_kdsp@live.com <kaique_kdsp@live.com>
2023-09-20 21:20:14 -0300

    Lógica para recuperar dados do usuário autenticado e configuração para manipular datas

--- src/main/java/br/com/exemplo/orientecaoObjetos/service/UserServiceImpl.java
+++ src/main/java/br/com/exemplo/orientecaoObjetos/service/UserServiceImpl.java
@@ -1,8 +1,13 @@
  package br.com.exemplo.orientecaoObjetos.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -37,7 +42,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User save(UserDto userDto) {
 		
-		// Algoritmo para criptografar a senha :  passwordEncoder
+	// Algoritmo para criptografar a senha :  passwordEncoder
 		
 		User user = new User(userDto.getFirstName(),
 				             userDto.getLastName(), 
@@ -45,15 +50,30 @@ public class UserServiceImpl implements UserService {
 				             passwordEncoder.encode(userDto.getPassword()),
 				             new ArrayList<>());
 		      userRepository.save(user);
-		      this.addRoleToUser(user.getEmail(), "ROLE_USER");
-		      return user;
+	this.addRoleToUser(user.getEmail(), "ROLE_USER");
+	return user;
 		      
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		User user = userRepository.findByEmail(username);
+		if (user==null) {
+			throw new UsernameNotFoundException("Invalid Username or password");
+		}
+			
+		return new org.springframework.security.core.userdetails.User(user.getEmail(),
+																  user.getPassword(),
+																  mapRoleToAuthorities(user.getRoles())
+																  );
+	}
+	
+	// Método responsável em trazer os papéis relacionados ao usuário
+	
+	
+	private Collection<? extends GrantedAuthority> mapRoleToAuthorities(Collection<Role> roles){
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
 	}
 
 	@Override
@@ -64,10 +84,26 @@ public class UserServiceImpl implements UserService {
 		userRepository.save(user);
 		
 	}
-
+	
 	@Override
 	public Role saveRole(Role role) {
 			return roleRepository.save(role);
+	}
+	
+	//Método responsável em buscar o usuario autenticado
+	@Override
+	public User getAuthenticatedUser() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username;
+		if(principal instanceof UserDetails) { 
+			username=((UserDetails)principal).getUsername();
+		}else {
+			username = principal.toString();
+		}
+		User user = userRepository.findByEmail(username);
+		
+		return user;
+		
 	}
 
 }
--- src/main/java/br/com/exemplo/orientecaoObjetos/wed/UserController.java
+++ src/main/java/br/com/exemplo/orientecaoObjetos/wed/UserController.java
@@ -1,17 +1,32 @@
 package br.com.exemplo.orientecaoObjetos.wed;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.exemplo.orientecaoObjetos.model.User;
+import br.com.exemplo.orientecaoObjetos.service.UserService;
 
 @Controller
 public class UserController {
 	
-	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
-
+	@GetMapping("/users/home")
+	public String homeUser(Model model) {
+		
+		String home = "index";
+		User user = userService.getAuthenticatedUser();
+		String username = user.getEmail();
+		model.addAttribute("username",username);
+		
+		return "home";
+	}
 }
 
